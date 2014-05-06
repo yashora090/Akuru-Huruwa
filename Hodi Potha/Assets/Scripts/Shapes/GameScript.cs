1fb46b76460a06958550aeef4017686ea9dfc843
yashora090 <thewunij@gmail.com>
2014-05-06 22:30:05 +0530

    5/6/2014 10:29 pm -shape scene one added

---
+++ Hodi Potha/Assets/Scripts/Shapes/GameScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class GameScript : MonoBehaviour {
+
+	private Dictionary<int, GameObject> trails = new Dictionary<int, GameObject>();
+
+
+	// Update is called once per frame
+	void Update () {
+
+		for (int i = 0; i < Input.touchCount; i++)
+		{
+
+			Touch touch = Input.GetTouch(i);
+			if (touch.phase == TouchPhase.Began)
+			{
+
+				if (trails.ContainsKey(i) == false)
+				{
+					Vector3 position = Camera.main.ScreenToWorldPoint(touch.position);
+					position.z = 0; 
+					
+					GameObject trail = SpecialEffectsScript.MakeballTrail(position);
+					
+					if (trail != null)
+					{
+						Debug.Log(trail);
+						trails.Add(i, trail);
+					}
+				}
+			}
+			else if (touch.phase == TouchPhase.Moved)
+			{
+
+				if (trails.ContainsKey(i))
+				{
+					GameObject trail = trails[i];
+					
+					Camera.main.ScreenToWorldPoint(touch.position);
+					Vector3 position = Camera.main.ScreenToWorldPoint(touch.position);
+					position.z = 0; 
+					
+					trail.transform.position = position;
+				}
+			}
+			else if (touch.phase == TouchPhase.Ended)
+			{
+				/*// Clear known trails
+				if (trails.ContainsKey(i))
+				{
+					GameObject trail = trails[i];
+					
+					// Let the trail fade out
+					Destroy(trail, trail.GetComponent<TrailRenderer>().time);
+					trails.Remove(i);
+				}*/
+			}
+
+	
+	}
+}
+}
---
+++ Hodi Potha/Assets/Scripts/Shapes/SpecialEffectsScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class SpecialEffectsScript : MonoBehaviour
+{
+	private static SpecialEffectsScript instance;
+	
+
+	public GameObject trailPrefab;
+	public GameObject ball;
+	
+	void Awake()
+	{
+		instance = this;
+	}
+	
+	/*void Start()
+	{
+		// Check prefabs
+
+		if (trailPrefab == null)
+			Debug.LogError("Missing Trail Prefab!");
+	}*/
+	
+
+
+	public static GameObject MakeballTrail(Vector3 position)
+	{
+		if (instance == null)
+		{
+			Debug.LogError("There is no SpecialEffectsScript in the scene!");
+			return null;
+		}
+		
+		GameObject ball = Instantiate(instance.trailPrefab) as GameObject;
+		ball.transform.position = position;
+
+		return ball;
+	}
+}
