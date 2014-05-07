f3edc06cf694b0ea1b3ce720782372589c9af13e
Lishani <lishani90@gmail.com>
2014-05-07 10:50:36 +0530

    puzzle added

---
+++ Hodi Potha/Assets/Scripts/puzzle/movepuz1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movepuz1 : MonoBehaviour {
+
+	public float speed = 0.1F;
+	void Update() {
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+			transform.Translate(touchDeltaPosition.x * speed, touchDeltaPosition.y * speed, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.Escape)) 
+			Application.Quit();
+
+	}
+}
