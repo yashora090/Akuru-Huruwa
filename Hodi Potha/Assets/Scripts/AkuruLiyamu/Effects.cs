85b9896f4e187912cef4cbf3ad8a4d80d557b3fd
Lishani <lishani90@gmail.com>
2014-05-07 12:29:26 +0530

    staged

--- Hodi Potha/Assets/Scripts/AkuruLiyamu/Effects.cs
+++ Hodi Potha/Assets/Scripts/AkuruLiyamu/Effects.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Effects : MonoBehaviour {
 
 	public Transform pointerPrefab;
+	private Object path;
 	// Use this for initialization
 	void Start () {
 
@@ -14,7 +15,8 @@ public class Effects : MonoBehaviour {
 		foreach (Touch touch in Input.touches) {
 			if(touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled) {
 				Vector3 touchPosition=Camera.current.ScreenToWorldPoint(touch.position);
-				Instantiate(pointerPrefab,touchPosition,Quaternion.identity);
+				touchPosition.z=0;
+				path=Instantiate(pointerPrefab,touchPosition,Quaternion.identity);
 			}
 
 		}
